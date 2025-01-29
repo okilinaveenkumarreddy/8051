cad5506183f9f5bae324f552ef904bbb6856407a
Okili Naveen Kumar Reddy <167627025+okilinaveenkumarreddy@users.noreply.github.com>
2025-01-29 18:41:27 +0530

    Update AL4leds.c

--- AL4leds.c
+++ AL4leds.c
@@ -1,6 +1,10 @@
-////blink the AL led at 1ms rate
+////Bitwise method of Controlling the IO Pins
+////Blinking 4 LEDS at a time
 //sfr P2=0xA0;
-//sbit LED=P2^0;
+//sbit LED1=P2^0;
+//sbit LED2=P2^1;
+//sbit LED3=P2^2;
+//sbit LED4=P2^3;
 //void delay(unsigned int ms){
 //	for(;ms>0;ms--){
 //		unsigned char i;
@@ -12,9 +16,9 @@
 //}
 //void main()
 //{ //pass the delay needed in ms
-//	LED=0;
+//	LED1=LED2=LED3=LED4=0;
 //	delay(1000);//calling fn takes 2 mc and returning to fn also takes 2 mc
-//	LED=1;
+//	LED1=LED2=LED3=LED4=1;
 //	delay(1000);
 //}
 /*Byte wise Methods */ //simple method to control IO Pins
@@ -22,7 +26,7 @@
 //then P2-4,5,6,7 pins are don't cares so we can write 0 or 1 for this bits
 
 sfr P2=0xA0;
-void delay(unsigned int ms){
+void delay(unsigned char ms){
 	for(;ms>0;ms--){
 		unsigned char i;
 		i=250;
@@ -33,8 +37,10 @@ void delay(unsigned int ms){
 }
 void main()
 { //pass the delay needed in ms
-	P2=0xF0;
-	delay(200);//calling fn takes 2 mc and returning to fn also takes 2 mc
-	P2=0xFF;
+//	P2=0xF0;
+//	delay(200);//calling fn takes 2 mc and returning to fn also takes 2 mc
+//	P2=0xFF;
+//	delay(200);
+	P2^=0x0F; //toggle method
 	delay(200);
 }
