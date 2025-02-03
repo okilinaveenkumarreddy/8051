8f0e41447b9a483b15a70ea8196955c46f5763dc
Okili Naveen Kumar Reddy <167627025+okilinaveenkumarreddy@users.noreply.github.com>
2025-02-03 17:54:45 +0530

    header.h

---
+++ Timers And Counters/header.h
@@ -0,0 +1,41 @@
+/*guarded macros */
+#ifndef __HEADER_H__
+#define __HEADER_H__
+/*Declare all the fns prototypes*/
+/*io_bit_write(2,0,1); I can understand this 2,0,1
+       since i wrote the program
+       but another guy can't understand
+      so define macros for port_num,pin_num,value    */
+/*ports*/
+//#define PORT0 0
+//#define PORT1 1
+//#define PORT2 2 
+//#define PORT3 3
+/*Pin_num*/
+//#define SW1_PIN 0
+//#define SW2_PIN 7
+/*values*/
+//#define SET 1
+//#define CLEAR 0
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned int u16;
+typedef signed int s16;
+typedef unsigned long int u32;
+typedef signed long int s32;
+extern void io_bit_write(u8 port_num,u8 pin_num,u8 val);
+extern bit io_bit_read(u8 port_num,u8 pin_num);
+extern void io_byte_write(u8 port_num,u8 byte);
+extern void delay_ms(u16 ms);
+extern void lcd_cmd(u8 cmd);
+extern void lcd_data(u8 d);
+extern void lcd_string(s8 *ptr);
+extern u8 string_len(s8 *s);
+extern void lcd_integer(s32 num);
+extern void lcd_float(float f);
+extern void lcd_init();
+extern u8 key2x3scan(void);
+extern u8 key4x4scan();
+
+
+#endif
